469b54404efa2e530d6c638d1279102cb7c0757c
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-04-05 18:50:13 +0200

    Add 2 comp Modal and backdrop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
-
+import Todo from './components/Todo';
 
 function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <h1>To doo list</h1>
+      <Todo text='Learning react' />
+      <Todo text='Learning Fast' />
+      <Todo text='Learning Smart' />
+    </div>
+  );
 }
 
 export default App;
