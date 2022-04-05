10d94241ddfdca9240786b96f83856ffd799102a
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-04-05 16:20:18 +0200

    del all

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React,  {useState} from "react";
-import TodoList from "./TodoList";
+
+
 
 function App() {
-   useState([])
-  return (
-    <>
-      <TodoList />
-      <input type="text" />
-      <button>Add to do</button>
-      <button>Clear</button>
-      <div>Ready</div>
-    </>
-  )
+  return <div>Hello</div>
 }
 
 export default App;
